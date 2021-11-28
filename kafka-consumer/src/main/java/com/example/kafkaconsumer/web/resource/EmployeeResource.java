d5b601e3ef8df8c5254efee87f68d27156916332
vbalaji215-dev <vbalaji215@outlook.com>
2021-11-28 10:10:22 -0500

    Added Controller to get all employees

---
+++ kafka-consumer/src/main/java/com/example/kafkaconsumer/web/resource/EmployeeResource.java
@@ -0,0 +1,11 @@
+package com.example.kafkaconsumer.web.resource;/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 28, November 2021
+ * Time: 10:04 AM
+ * Project: json-example
+ * Package Name: com.example.kafkaconsumer.web.resource
+ * To change this template use File | Settings | File and Code Template
+ */
+    public class EmployeeResource {
+}

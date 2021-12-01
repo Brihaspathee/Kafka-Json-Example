a33a8dc8daf29f2f2c3d58c7307443a4c77d520d
vbalaji215-dev <vbalaji215@outlook.com>
2021-12-01 15:52:27 -0500

    Updated Docker compose to use bind mounts

--- kafka-consumer/src/main/java/com/example/kafkaconsumer/web/resource/EmployeeResource.java
+++ kafka-consumer/src/main/java/com/example/kafkaconsumer/web/resource/EmployeeResource.java
@@ -1,4 +1,17 @@
-package com.example.kafkaconsumer.web.resource;/**
+package com.example.kafkaconsumer.web.resource;
+
+import com.example.kafkaconsumer.domain.entity.Employee;
+import com.example.kafkaconsumer.service.interfaces.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
  * Created in Intellij IDEA
  * User: Balaji Varadharajan
  * Date: 28, November 2021
@@ -7,5 +20,16 @@ package com.example.kafkaconsumer.web.resource;/**
  * Package Name: com.example.kafkaconsumer.web.resource
  * To change this template use File | Settings | File and Code Template
  */
-    public class EmployeeResource {
+@RestController
+@RequestMapping("/api/v1/employee")
+@RequiredArgsConstructor
+public class EmployeeResource {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity getAllEmployees(){
+        List<Employee> employees = employeeService.getAllEmployees();
+        return new ResponseEntity(employees, HttpStatus.OK);
+    }
 }
